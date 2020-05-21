5ba9ecfd6f963aa4254ce30dd3e26522dfec7d08
michael thomas <michael.thomas2014@gmail.com>
2020-05-21 07:36:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,6 +3,15 @@ class Transfer
   attr_reader :sender, :receiver
   attr_accessor :status, :transfer_amount
   
+  def initialize(sender, receiver, transfer_amount)
+    @sender = sender
+    @receiver = receiver
+    @transfer_amount = transfer_amount
+    @status = "pending"
+  end
+  
+  
+  
   
   
 end
