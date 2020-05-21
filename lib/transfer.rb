9d90a044591955c07c128bf16508c856036d6ef4
michael thomas <michael.thomas2014@gmail.com>
2020-05-21 08:00:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.balance >= @amount && 
+    if @sender.balance >= @amount && @sender.status == "open"
       @sender.withdraw(@amount) 
       @receiver.deposit(@amount)
       @status = "complete"
